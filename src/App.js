94d30ca8a968155c1237f3b5a6804c83912464db
EZZ YASER <zoz966737@gmail.com>
2024-11-18 19:13:16 +0200

    install Tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
-
+import { Page } from './component/Page';
 function App() {
   return (
     <div className="App">
       <h1>Hello every body</h1>
+      <Page name="EZZ" />
+      <Page name="YOUSEF" />
+      <Page name="AMMAR" />
+      <Page name="SPHIB " />
     </div>
   );
 }
---
+++ src/component/Page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const  Page = (props) => {
+    return (
+        <div>
+            <h1>{props.name}</h1>
+        </div>
+    )
+}
+
+// import React from 'react'
+
+// export const Page = () => {
+//   return (
+//     <div>Page</div>
+//   )
+// }
